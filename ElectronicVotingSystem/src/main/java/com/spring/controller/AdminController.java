d7832dc14b4385ff68f01c2a62951ca2cb6ed5f8
apeksha2403 <60914765+apeksha2403@users.noreply.github.com>
2020-05-02 10:51:05 +0530

    Add and View Candidate Details

--- ElectronicVotingSystem/src/main/java/com/spring/controller/AdminController.java
+++ ElectronicVotingSystem/src/main/java/com/spring/controller/AdminController.java
@@ -1,24 +1,27 @@
 package com.spring.controller;
 
-import java.time.LocalDate;
 import java.util.List;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.format.annotation.DateTimeFormat.ISO;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.entity.ApplicationEntity;
+import com.spring.entity.CandidateEntity;
 import com.spring.entity.ElectionEntity;
 import com.spring.entity.PartyEntity;
 import com.spring.entity.UserCredentialsEntity;
+import com.spring.entity.UserEntity;
+import com.spring.json.Application;
+import com.spring.json.Candidate;
 import com.spring.json.Election;
 import com.spring.json.LoginResponse;
 import com.spring.json.Party;
@@ -38,35 +41,56 @@ public class AdminController {
 	public Object addElectionDetails(@RequestBody Election election, ElectionEntity electionEntity, @RequestHeader(name = "sessionId") String sessionId) {
 		BeanUtils.copyProperties(election, electionEntity);
 		return adminService.addElection(electionEntity,sessionId);
-	//	return electionEntity;
-	}
-	@PostMapping(value="/party")
-	public Object  addPartyDetails(@RequestBody Party party,PartyEntity partyEntity,@RequestHeader(name="sessionId") String sessionId){
-	 BeanUtils.copyProperties(party, partyEntity);
-		return adminService.addParty(partyEntity,sessionId);
-		
 	}
 	
 	
+	@PostMapping("/election/{electionid}/party/{partyid}/candidate")
+	public Object addCandidateDetails(@RequestBody Candidate candidate, CandidateEntity candidateEntity, @RequestHeader(name = "sessionId")
+	String sessionId,@PathVariable(name="electionid") String electionid,@PathVariable(name="partyid") String partyid) {
+		BeanUtils.copyProperties(candidate, candidateEntity);
+		return adminService.addCandidate(candidateEntity,sessionId,electionid);
+	}
 	
-
-
+	@PostMapping(value="/party", produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public PartyEntity  addPartyDetails(@RequestBody Party party,PartyEntity partyEntity)
+	{ BeanUtils.copyProperties(party, partyEntity);
+		 adminService.addParty(partyEntity, sessionId);
+		return partyEntity;
+	}
+	
+	
 	@GetMapping("/election")
 	public Object getElectionDetails(@RequestHeader(name = "sessionId") String sessionId) {
 		return adminService.getElectionDetails(sessionId);
 	}
+
+	
+	
+	@GetMapping("/candidate")
+	public Object getCandidateDetails(@RequestHeader(name = "sessionId") String sessionId
+			) {
+		return adminService.getCandidateDetails(sessionId);
+	}
 	
 	@GetMapping(value="/party")
-		public Object getPartyDetails(@RequestHeader(name="sessionId") String sessionId){
+		public List<PartyEntity> getPartyDetails(){
 			return adminService.getPartyDetails(sessionId);
 		}
-	
 		
-		  @GetMapping("/election/fromDate/{date}")
-		  public Object getElectionFromElectionDate(@PathVariable(value="date") @DateTimeFormat(iso = ISO.DATE) LocalDate date,@RequestHeader(name="sessionId") String sessionId) {
-		  return adminService.getAllElectionFromElectionDate( date, sessionId); }
-		 
+	@GetMapping(value="/application/request")
+	public List<ApplicationEntity> getRequest(){
+		return adminService.getRequest(sessionId);
+	}
 	
+	@PostMapping(value="/application/{userid}/update",produces=MediaType.APPLICATION_JSON_VALUE, consumes=MediaType.APPLICATION_JSON_VALUE)
+	public Object updateRequest(@RequestBody ApplicationEntity 
+			applicationEntity,Application
+			application,@RequestHeader(name = "sessionId") String sessionId,
+			@PathVariable(name="userid") String userid
+			){
+       BeanUtils.copyProperties(application,applicationEntity);
+		return adminService.updateRequest(applicationEntity,userid,sessionId);
+	}
 
 	@PostMapping("/login")
 	public LoginResponse loginAdmin(@RequestBody UserCredentials userCredentials,
